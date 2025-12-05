dd9c01457c0fe1a948fab94d6206ac770488a0e0
eoin <eoin.corrigan.j94981@mcast.edu.mt>
2025-12-05 15:26:46 +0100

    Third Update
    - Boxcollider added to money prefab
    - CashSpawner created which moves L/R
    - CashShredder created
    - CashShredder script created to destroy missed pickups.

---
+++ Assets/Scripts/CashSpawner.cs
@@ -0,0 +1,52 @@
+ using UnityEngine;
+using System.Collections;
+
+public class CashSpawner : MonoBehaviour
+{
+
+    [SerializeField] int amountToSpawn = 10;
+    [SerializeField] float maxTimeBetweenSpawn = 10f;
+    [SerializeField] GameObject cashPrefab;
+    [SerializeField] float borderL = -8f;
+    [SerializeField] float borderR = 8f;
+    [SerializeField] float spawnerSpeed = 5f;
+    [SerializeField] float cashFallSpeed = 3f;
+    float direction = 1f;
+    void Start()
+    {
+        StartCoroutine(SpawnCash());
+    }
+
+    IEnumerator SpawnCash()
+    {
+        for(int i=0;i<amountToSpawn;i++) {
+            yield return new WaitForSeconds(Random.Range(0f, maxTimeBetweenSpawn));
+            float xPosition = Random.Range(borderL, borderR);
+            Vector3 spawnPosition = new Vector3(xPosition, 9, 0f);
+
+            GameObject cash = Instantiate(
+                cashPrefab,
+                spawnPosition,
+                Quaternion.identity
+            );
+            cash.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(0, -cashFallSpeed);
+        }
+    }
+    void Update()
+    {
+        float newX = transform.position.x + direction * spawnerSpeed * Time.deltaTime;
+        
+        if (newX >= borderR)
+        {
+            newX = borderR;
+            direction = -1f;
+        }
+        else if (newX <= borderL)
+        {
+            newX = borderL;
+            direction = 1f;
+        }
+        
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+    }
+}
